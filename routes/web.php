8eee86bf01a04d05432bf7d9573dce26dfbd4343
MuhammadAdan2003 <adankhan7770@gmail.com>
2025-09-08 18:01:42 +0500

    Some Changes

--- routes/web.php
+++ routes/web.php
@@ -34,21 +34,18 @@ Route::get('/login', function () {
     return view('Users.Auth.login');
 })->name('showLogin');
 
-// Route::get('/trips', function () {
- 
-// });
-
+// ============ TRIP ROUTES ============ //
 Route::get('/addTrip', [TripController::class, 'index'])->name('viewFormTrip');
 Route::get('/trips', [TripController::class, 'trips'])->name('showtrips');
 Route::post('/trips', [TripController::class, 'store'])->name('tripstore');
 Route::get('/mytrip/{id}', [TripController::class, 'trip'])->name('myttrip');
 
-
+// ========== AUTH ROUTES ========= //
 Route::post('/signup', [AuthController::class, 'register'])->name('signup');
 Route::post('/signin', [AuthController::class, 'login'])->name('loginn');
 Route::get('/logout', [AuthController::class, 'logout'])->name('logouts');
 
-
+// ========== EXPENSE ROUTES ========= //
 Route::post('addExp', [ExpenseController::class , 'addExp'])->name('addingExp');
 Route::get('getExp/{trip_id}', [ExpenseController::class , 'exp'])->name('gettingExp');
 Route::delete('/delTrip/{expense_id}' , [ExpenseController::class , 'destroy'])->name('deleteExp');
